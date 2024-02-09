f1a31149acc4df87f8a4c7cd3fa0d24e7a622fed
Arthur serenne <arthur.serenne@gmail.com>
2024-02-09 14:43:34 +0100

    add images

--- slick.js
+++ slick.js
@@ -29,3 +29,31 @@ if(jQuery('.carousel_1').length) {
     
   });
 }
+
+if(jQuery('.carousel_2').length) {
+  jQuery('.carousel_2').slick({
+    centerPadding: '0px',
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    arrows: false,
+    responsive: [
+    {
+      breakpoint: 770,
+      settings: {
+        slidesToShow: 3 
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }],
+
+    
+  });
+}
